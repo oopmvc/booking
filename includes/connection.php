fd4cf1debae73982a9df9c4cce931a95bfe0b77e
webkomagency <webkomagency@gmail.com>
2018-07-27 18:09:19 +0200

    remove procedural db connection

--- includes/connection.php
+++ includes/connection.php
@@ -5,16 +5,6 @@
     define('DB_PASSWORD', 'admin');
     define('DB_NAME', 'mauriziobarbershop');
 
-    /* Attempt to connect to MySQL database */
-    //$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-    // Check connection
-    /*if($link === false){
-        die("ERROR: Connessione non riuscita. " . mysqli_connect_error());
-    }*/
-
-
-
     try {
         /* Attempt to connect to MySQL database */
         $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
