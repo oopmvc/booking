b02fe21275857c9c7d2d28d1d09b080a700e7ae7
webkomagency <webkomagency@gmail.com>
2018-07-27 18:25:39 +0200

    add product page

--- product-create.php
+++ product-create.php
@@ -11,6 +11,19 @@
     <div class="row">
         <div class="col-lg-12">
             <h1>Crea Servizio</h1>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                <label for="name">Nome</label>
+                <input type='text' name='name' class='form-control' />
+
+                <label for="">Descrizione</label>
+                <textarea name='description' class='form-control'></textarea></td>
+
+                <label for="">Price</label>
+                <input type='text' name='price' class='form-control' /></td>
+                <input type='submit' value='Save' class='btn btn-primary' />
+
+                <a href='index.php' class='btn btn-danger'>Back to read products</a>
+            </form>
         </div>
     </div>
 </div>
